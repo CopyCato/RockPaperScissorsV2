2d3639d2ccbf4392b46d9f88508f4d36b8c61885
CopyCato <nikola93nikolov@gmail.com>
2023-02-03 16:59:01 +0100

    Changed order of functions

--- JS.js
+++ JS.js
@@ -4,24 +4,26 @@ let playerSelection ='';
 let computerSelection = '';
 let result = '';
 
+
+function getPlayerChoice(){
+  let selection = prompt("Please enter Rock, Paper or Scissors");
+  return selection.toLowerCase();
+}
+
+function getComputerChoice(){
+  let choice = Math.floor(Math.random() * 3)
+  if(choice == 0){
+    return 'rock';
+  }else if(choice == 1){
+    return 'scissors';
+  }else return 'paper';
+}
+
 function playRound(computerSelection, playerSelection){
-
-  function getPlayerChoice(){
-    let selection = prompt("Please enter Rock, Paper or Scissors");
-    return selection.toLowerCase();
-  }
-
-  function getComputerChoice(){
-    let choice = Math.floor(Math.random() * 3)
-    if(choice == 0){
-      return 'rock';
-    }else if(choice == 1){
-      return 'scissors';
-    }else return 'paper';
-  }
-
   playerSelection = getPlayerChoice();
   computerSelection = getComputerChoice();
+  console.log('Player\'s choice is', playerSelection);
+  console.log('Computer\'s choice is', computerSelection);
 
   if(computerSelection === playerSelection){
     return 'tie';
